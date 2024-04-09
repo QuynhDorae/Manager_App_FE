c93a5096cd9c28921ba501b4aecbbe5391f11ce3
quynhdt <quynhdt@gct.com.vn>
2024-04-09 20:24:57 +0700

    ok

---
+++ App_Admin/pages/logout.vue
@@ -0,0 +1,22 @@
+<script setup>
+async function logoutUser() {
+    try {
+        const response = await api.post('auth/logout');
+        useCookie('token').value = null
+        useCookie('refreshToken').value = null
+        useCookie('userId').value = null
+        console.log(response.data);
+        navigateTo('/')
+    } catch (error) {
+        console.error('Error logging out:', error);
+        // Xử lý lỗi nếu cần
+    }
+}
+onMounted(async () => {
+    await logoutUser();
+});
+</script>
+
+<template>
+
+</template>
